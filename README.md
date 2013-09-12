c6ee6999ad287628f7012ecf1f28b1902e68cd00
Michael Geers <michael.geers@neuland-bfi.de>
2013-09-12 16:57:54 +0200

    added support for including non jade files inside of templates; version bump

--- README.md
+++ README.md
@@ -78,7 +78,7 @@ We are using Github for Maven hosting. Just add this repository ...
 <dependency>
   <groupId>de.neuland</groupId>
   <artifactId>spring-jade4j</artifactId>
-  <version>0.3.2</version>
+  <version>0.3.3</version>
 </dependency>
 ```
 
--- src/main/java/de/neuland/jade4j/spring/template/SpringTemplateLoader.java
+++ src/main/java/de/neuland/jade4j/spring/template/SpringTemplateLoader.java
@@ -6,6 +6,7 @@ import java.io.Reader;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.apache.commons.io.FilenameUtils;
 
 import de.neuland.jade4j.template.TemplateLoader;
 import javax.annotation.PostConstruct;
@@ -55,11 +56,15 @@ public class SpringTemplateLoader implements TemplateLoader, ServletContextAware
 
 	private Resource getResource(String name) {
 		String resourceName = basePath + name;
-		if (!resourceName.endsWith(suffix)) {
+        if (hasNoExtension(resourceName)) {
 			resourceName += suffix;
 		}
 		return this.resourceLoader.getResource(resourceName);
 	}
+
+    private boolean hasNoExtension(String filename) {
+        return "".equals(FilenameUtils.getExtension(filename));
+    }
 
 	public String getEncoding() {
 		return encoding;
